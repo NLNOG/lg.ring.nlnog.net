8524d8880e6e292010ae157fe0af1e242c3961f7
Teun Vink <teun@teun.tv>
2022-09-20 09:46:28 +0200

    add license

--- nlnog_lg.py
+++ nlnog_lg.py
@@ -2,10 +2,26 @@
 # -*- coding: utf-8 -*-
 
 """
-Looking Glass code for the NLNOG Looking Glass.
-Written by Teun Vink <nlnog@teun.tv>
+ Looking Glass code for the NLNOG Looking Glass.
+ Written by Teun Vink <nlnog@teun.tv>
 
-Source code: https://github/com/NLNOG/nlnog-lg
+ Source code: https://github/com/NLNOG/nlnog-lg
+
+
+ Copyright (c) 2022 Stichting NLNOG <stichting@nlnog.net>
+
+ Permission to use, copy, modify, and distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ 
 """
 
 import re
